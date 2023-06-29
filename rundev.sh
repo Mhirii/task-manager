62cade30c13a129cc3741a371b63852dcaaa3c09
Mhirii <ahmedmhiri218@gmail.com>
2023-06-29 13:20:39 +0100

    Initial commit

---
+++ rundev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+server1_dir="./backend"
+server2_dir="./frontend"
+
+start_server() {
+  local server_dir=$1
+  local start_command=$2
+
+  echo "Starting server in $server_dir"
+  kitty --directory="$server_dir" fish -c "$start_command"
+}
+
+start_server "$server1_dir" "npm run start:dev" &
+
+start_server "$server2_dir" "npm run dev" &
+
+cd "$server2_dir
