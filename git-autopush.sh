ae8f3647ec337ce4d133daf5700b884d3c06517d
Mhirii <ahmedmhiri218@gmail.com>
2023-06-29 13:39:23 +0100

    test

--- git-autopush.sh
+++ git-autopush.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "PROVIDE COMMIT MESSAGE!"
+  exit 1
+fi
+
+message="$1"
 repo="https://github.com/Mhirii/task-manager"
 branch="main"
 
 git add .
 
-git commit -m "Auto"
+git commit -m "$message"
 
 git push $repo $branch
