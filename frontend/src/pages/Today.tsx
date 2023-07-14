d4126480aba4c8d64a344b95cf10c799cee9a575
Ahmed Mhiri <ahmedmhiri218@gmail.com>
2023-07-14 18:16:21 +0100

    code cleaning

--- frontend/src/pages/Today.tsx
+++ frontend/src/pages/Today.tsx
@@ -29,33 +29,21 @@ export default function Today() {
 
   return (
     <div className=" p-1 w-full flex  justify-center">
+      
       <div className="grid grid-cols-1 gap-1 sm:w-4/5 lg:w-2/3 w-full">
         <TaskBoard data={taskData} view="list" />
-        {/* 
-        <TaskCard
-          title="Task 1"
-          notes="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam suscipit deleniti fugit nihil aliquid tenetur quis corporis quam neque? Officiis?"
-          dueDate="2023-07-24"
-          project="project 1"
-          id="1"
-        />
-        <TaskCard
-          title="Task 2"
-          desc="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam suscipit deleniti fugit nihil aliquid tenetur quis corporis quam neque? Officiis?"
-          id="2"
-          dueDate="2023-07-06"
-        /> 
-        */}
         <button className="h-12 w-full hidden sm:flex flex-row justify-center items-center ">
           <PlusIcon className="h-5 w-5 text-lime-600" />
           <p className="text-sm font-regular mx-1 text-slate-600">Add task</p>
         </button>
       </div>
+      
       <Button
         icon={<PlusIcon className="w-6 h-6 text-slate-100" />}
         // label="Add"
         className="rounded-full w-12 h-12 fixed z-10 bottom-2 right-2 sm:hidden"
       />
+      
     </div>
   );
 }
