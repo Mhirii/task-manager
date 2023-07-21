79c3bca8c5c57e5c0c39b8d22df293d4506d5405
Mhiri <ahmedmhiri218@gmail.com>
2023-07-21 13:40:08 +0100

    cleanup

--- frontend/src/components/TaskBoard.tsx
+++ frontend/src/components/TaskBoard.tsx
@@ -39,7 +39,6 @@ export default function TaskBoard({/*data,*/ view}: props) {
       setTasks(response.data);
     });
   }, []);
-  let board: any;
   // console.log(tasks)
 
   if (!tasks) return null;
