37a78f970a1477f1f2092c48b45bb3e2755a1d6e
Mhiri <ahmedmhiri218@gmail.com>
2023-07-21 09:28:15 +0100

    cleanup

--- frontend/src/pages/Today.tsx
+++ frontend/src/pages/Today.tsx
@@ -1,36 +1,12 @@
-import { PlusIcon } from "@heroicons/react/24/solid";
 import TaskBoard from "../components/TaskBoard";
-import Button from "../components/common/Button";
-import tasks from "../dataSample/tasks.json";
 import AddTask from "../components/AddTask.tsx";
-interface projectRef {
-  id: string;
-  title: string;
-  color: string;
-}
 
-interface Task {
-  _id: {
-    $oid: string;
-  };
-  title: string;
-  desc: string;
-  project?: projectRef;
-  dateAdded: {
-    $date: string;
-  };
-  due: {
-    $date: string;
-  };
-  isDone: boolean;
-}
 
 interface props {
   view:string;
 }
 
 export default function Today({view}:props) {
-  const taskData: Task[] = tasks as Task[];
 
   return (
     <div className=" p-1 w-full flex  justify-center">
