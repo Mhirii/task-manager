e69cec797ee30b872db1c20fe17f94252b7fcc0c
Mhiri <ahmedmhiri218@gmail.com>
2023-07-21 09:12:33 +0100

    added onclick

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,26 +1,45 @@
-import { ReactNode } from "react";
-
+import {ReactNode, useState} from "react";
+import "../../styles/button.css";
 interface ButtonProps {
   icon?: ReactNode;
   label?: string;
   className?: string;
+  variant?: string;
+  onClick?: (id: string) => void;
+  id?: string;
 }
 
-export default function Button({ icon, label, className }: ButtonProps) {
+export default function Button({ icon, label, className, variant,onClick, id}: ButtonProps) {
+
   let gap: string;
   if (icon === undefined || label === undefined) {
     gap = "gap-0";
   } else {
     gap = "gap-2";
   }
+  let variantStyle: string = "btn-primary"
+  if(variant === "danger" ){
+    variantStyle = "btn-danger"
+  }
+  else if(variant === "confirm" ){
+    variantStyle = "btn-confirm"
+  }
+  else if(variant === "transparent" ){
+    variantStyle = "btn-transparent"
+  }
+  else if(variant === "ghost" ){
+    variantStyle = "btn-ghost"
+  }
+
   return (
     <button
-      className={`flex flex-row items-center justify-between py-2 px-4 
-        bg-lime-500 rounded-lg ${gap} ${className}
-        `}
+      className={`flex flex-row items-center justify-between py-2 px-4  
+        ${variantStyle} rounded-lg ${gap} ${className}
+      `}
+      onClick={()=> onClick(id)}
     >
       {icon}
-      <p className="text-xl text-slate-100">{label}</p>
+      <p className={`text-xl ${variantStyle}`}>{label}</p>
     </button>
   );
 }
