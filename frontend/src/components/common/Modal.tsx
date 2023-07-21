ecae45ca35e1d3f047845e897e1a68a5c5b19709
Mhiri <ahmedmhiri218@gmail.com>
2023-07-21 12:32:30 +0100

    responsiveness stuff

--- frontend/src/components/common/Modal.tsx
+++ frontend/src/components/common/Modal.tsx
@@ -24,11 +24,11 @@ export function Modal({header, body, footer, state, toggleState, posInit, posHid
         transition-opacity delay-0 duration-300
         `}>
 
-        <div className={` task-modal-overlay w-1/4 h-full`} onClick={toggleState}></div>
+        <div className={` task-modal-overlay w-1/4 md:w-full h-full`} onClick={toggleState}></div>
 
         <div className={` task-modal
           backdrop-blur bg-slate-200 bg-opacity-75
-          h-full w-3/4
+          h-full w-3/4 md:w-96
            ${state ? "-translate-x-0" : "translate-x-full opacity-0"}
            transition-transform delay-0 duration-300
            flex flex-col p-4 items-center gap-2`
