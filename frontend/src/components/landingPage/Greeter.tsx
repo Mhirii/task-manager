7668f962089f992c500b7c8129fa99c61d4de3b9
Ahmed Mhiri <ahmedmhiri218@gmail.com>
2023-07-26 19:14:41 +0100

    added parallax

---
+++ frontend/src/components/landingPage/Greeter.tsx
@@ -0,0 +1,8 @@
+
+export default function Greeter() {
+    return (
+      <div className="greeter">
+          <h1>Embrace the Art of Endeavour with Endeavour - Your Task Management Companion</h1>
+      </div>
+    );
+}
