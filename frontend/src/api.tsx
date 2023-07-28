57ec6cd3eb177e8de6d6fe91e2049528f019458b
Ahmed Mhiri <ahmedmhiri218@gmail.com>
2023-07-28 15:39:07 +0100

    to easily toggle backend : localhost / vercel

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,7 +1,8 @@
 
 
 
-const URL   = "https://taskmanager-backend.vercel.app/";
+// const URL   = "https://taskmanager-backend.vercel.app/";
 
-// const URL   = "http://localhost:5000/";
+const URL   = "http://localhost:5000/";
 export const AllTasksURL: string = URL+"tasks/";
+export const authLogin: string = URL+"auth/login";
