c57533b4f375899b0c9ed4f69265c6f2633775c8
Ahmed Mhiri <ahmedmhiri218@gmail.com>
2023-08-06 12:22:04 +0100

    [fix][0.0.2] enabled CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Endeavour TaskManager')
