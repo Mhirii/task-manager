a266cf83bdd62d63884183c29c7957d92de41be1
Ahmed Mhiri <ahmedmhiri218@gmail.com>
2023-08-13 19:30:03 +0100

    [feature][0.0.10] added charts
    very buggy

---
+++ frontend/src/components/charts/BarChart.tsx
@@ -0,0 +1,27 @@
+import { Bar } from "react-chartjs-2";
+
+interface Props{
+  chartData: any
+}
+const BarChart = ({ chartData }: Props) => {
+  return (
+    <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Bar Chart</h2>
+      <Bar
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Users Gained between 2016-2020"
+            },
+            legend: {
+              display: false
+            }
+          }
+        }}
+      />
+    </div>
+  );
+};
+export default BarChart
